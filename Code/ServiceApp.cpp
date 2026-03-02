454f7efe15d82420c282c41effaf8bb8df504f47
pelevich <76266728+pelevich@users.noreply.github.com>
2026-03-02 20:56:26 +0300

    Add files via upload

---
+++ Code/ServiceApp.cpp
@@ -0,0 +1,31 @@
+﻿// CMakeServiceApp.cpp: определяет точку входа для приложения.
+//
+
+#include "ServiceApp.h"
+
+using namespace std;
+
+int main()
+{
+    Service object("MyPipe");
+
+    bool State = object.ConnectUser(object.hPipe);
+
+    if (!State) {
+        cout << "Couldn't connect" << endl;
+        return 0;
+    }
+
+    State = object.Read(object.hPipe, object.servBuff, sizeof(object.servBuff));
+
+    if (!State) {
+        cout << "Couldn't read it" << endl;
+    }
+
+    State = object.Write(object.hPipe, object.send_message);
+    if (!State) {
+        cout << "Couldn't send" << endl;
+    }
+
+    return 0;
+}
---
+++ Code/ServiceApp.h
@@ -0,0 +1,13 @@
+﻿// CMakeServiceApp.h : включаемый файл для стандартных системных включаемых файлов
+// или включаемые файлы для конкретного проекта.
+
+#pragma once
+
+#include <iostream>
+#include <windows.h>
+#include <vector>
+#include "Service.h"
+
+using namespace std;
+
+// TODO: установите здесь ссылки на дополнительные заголовки, требующиеся для программы.
